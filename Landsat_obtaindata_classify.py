55bb8d66bf375c3c7f822597d279f403fa53b2ca
f.trotter <felix.trotter86@gmail.com>
2024-02-29 19:09:56 +0000

    Update Landsat_obtaindata_classify.py

--- Landsat_obtaindata_classify.py
+++ Landsat_obtaindata_classify.py
@@ -51,15 +51,17 @@ df_scenes = pd.DataFrame(scenes)
 df_scenes = df_scenes[['display_id','wrs_path', 'wrs_row','satellite','cloud_cover','acquisition_date']]
 df_scenes.sort_values('acquisition_date', ascending=False, inplace=True)
 
+# set main directory
 directory = '/Users/felixtrotter/Documents/Training/Projects/GGE_Solar/Data_samples'
 
+# load EarthEcplorer from landsat package
 from landsatxplore.earthexplorer import EarthExplorer
 import os
 
 # Initialize the API
 ee = EarthExplorer(username, password)
 
-
+# loop through each satellite file stored in the dataframe
 for i in df_scenes["display_id"]:
     print(i)
     # Select the first scene
@@ -79,11 +81,13 @@ for i in df_scenes["display_id"]:
 
 ee.logout()
 
-
+# import package to extract data from compressed tar folder
 import tarfile
 
+# loop through each file
 for file in os.listdir(directory):
     print(file)
+    # ignore hidden files
     if not file.endswith('DS_Store'):
         # Extract files from tar archiveßß
         tar = tarfile.open(os.path.join(directory, file))
@@ -91,9 +95,61 @@ for file in os.listdir(directory):
         tar.close()
 
 
+# we need to give each satelite image that we create from the bands a 
+# CRS. Here we use EPSG 4326 = WGS84
+# load packages
+from rasterio.merge import merge
+from rasterio.transform import from_origin
+from rasterio.enums import Resampling
+from rasterio.plot import show
+from rasterio.plot import show_hist
+from rasterio.plot import plotting_extent
+from rasterio.mask import mask
+from rasterio.warp import calculate_default_transform, reproject, Resampling
+
+
+# set crs
+dst_crs = 'EPSG:4326'
+
+# create function that can reproject rasters
+def reproject_raster(in_path, out_path):
+# reproject raster to project crs
+    with rasterio.open(in_path) as src:
+        src_crs = src.crs
+        transform, width, height = calculate_default_transform(src_crs, dst_crs, src.width, src.height, *src.bounds)
+        kwargs = src.meta.copy()
+
+        kwargs.update({
+            'crs': dst_crs,
+            'transform': transform,
+            'width': width,
+            'height': height})
+
+        with rasterio.open(out_path, 'w', **kwargs) as dst:
+            for i in range(1, src.count + 1):
+                reproject(
+                    source=rasterio.band(src, i),
+                    destination=rasterio.band(dst, i),
+                    src_transform=src.transform,
+                    src_crs=src.crs,
+                    dst_transform=transform,
+                    dst_crs=dst_crs,
+                    resampling=Resampling.nearest)
+        return(out_path)
+
+
+
+
 import tifffile as tiff
 import numpy as np
 import matplotlib.pyplot as plt
+import rioxarray
+from affine import Affine
+import mercantile
+
+import rasterio
+from rasterio.enums import Resampling
+from rasterio.vrt import WarpedVRT
 
 # create directory oif not yet exists
 dir_RGB_images = os.path.join(directory, 'RGB_images')
@@ -104,6 +160,7 @@ RBG_ls = []
 for ii in df_scenes["display_id"]:
     print(ii)
     # Select the first scene
+    ii = "LC09_L2SP_197024_20230826_20230828_02_T1"
     ID = ii
     # Load Blue (B2), Green (B3) and Red (B4) bands
     B2 = tiff.imread(os.path.join(directory, '{}_SR_B2.TIF').format(ID, ID))
@@ -117,8 +174,50 @@ for ii in df_scenes["display_id"]:
     # Clip to enhance contrast
     RGB = np.clip(RGB,0,0.2)/0.2
 
+    # write CRS
+    #RGB.rio.write_crs("epsg:4326", inplace=True)
+
     # save image as tif file in new directory
     tiff.imwrite(dir_RGB_images + '/' + 'RGB__' + ii + '.tif', RGB)
+
+    #rds = rioxarray.open_rasterio(dir_RGB_images + '/' + 'RGB__' + ii + '.tif')
+    #rds.rio.write_crs("epsg:4326", inplace=True)
+    #rds_4326 = rds.rio.reproject("EPSG:4326")
+    #rds_4326.rio.to_raster(dir_RGB_images + '/' + 'RGB__' + ii + '.tif', RGB)
+    with rasterio.open(dir_RGB_images + '/' + 'RGB__' + ii + '.tif') as src:
+        with WarpedVRT(src, crs='EPSG:4326',
+                    resampling=Resampling.bilinear) as vrt:
+
+            # Determine the destination tile and its mercator bounds using
+            # functions from the mercantile module.
+            dst_tile = mercantile.tile(*vrt.lnglat(), 9)
+            left, bottom, right, top = mercantile.xy_bounds(*dst_tile)
+
+            # Determine the window to use in reading from the dataset.
+            dst_window = vrt.window(left, bottom, right, top)
+
+            # Read into a 3 x 512 x 512 array. Our output tile will be
+            # 512 wide x 512 tall.
+            data = vrt.read(window=dst_window, out_shape=(3, 512, 512))
+
+            # Use the source's profile as a template for our output file.
+            profile = vrt.profile
+            profile['width'] = 512
+            profile['height'] = 512
+            profile['driver'] = 'GTiff'
+
+            # We need determine the appropriate affine transformation matrix
+            # for the dataset read window and then scale it by the dimensions
+            # of the output array.
+            dst_transform = vrt.window_transform(dst_window)
+            scaling = Affine.scale(dst_window.height / 512,
+                                dst_window.width / 512)
+            dst_transform *= scaling
+            profile['transform'] = dst_transform
+
+            # Write the image tile to disk.
+            with rasterio.open(dir_RGB_images + '/' + 'RGB__' + ii + '.tif', 'w', **profile) as dst:
+                dst.write(data)
 
     # append each RGB image to the list
     #RBG_ls.append(RGB)
@@ -130,8 +229,81 @@ for ii in df_scenes["display_id"]:
 
 
 
-for img in RBG_ls:
-    tiff.imsave(dir_RGB_images)
+
+
+
+ii = "LC09_L2SP_197024_20230826_20230828_02_T1"
+ID = ii
+# Load Blue (B2), Green (B3) and Red (B4) bands
+B2 = tiff.imread(os.path.join(directory, '{}_SR_B2.TIF').format(ID, ID))
+B3 = tiff.imread(os.path.join(directory, '{}_SR_B3.TIF').format(ID, ID))
+B4 = tiff.imread(os.path.join(directory, '{}_SR_B4.TIF').format(ID, ID))
+
+# Stack and scale bands
+RGB = np.dstack((B4, B3, B2))
+RGB = np.clip(RGB*0.0000275-0.2, 0, 1)
+
+# Clip to enhance contrast
+RGB = np.clip(RGB,0,0.2)/0.2
+
+# write CRS
+#RGB.rio.write_crs("epsg:4326", inplace=True)
+
+# save image as tif file in new directory
+tiff.imwrite(dir_RGB_images + '/' + 'RGB__' + ii + '.tif', RGB)
+
+
+
+from osgeo import gdal
+
+file = dir_RGB_images + '/' + 'RGB__' + ii + '.tif'
+outFileName = dir_RGB_images + '/' + 'RGB__' + ii + '__mod' + '.tif'
+ds = gdal.Open(file)
+band = ds.GetRasterBand(1)
+arr = band.ReadAsArray()
+[rows, cols] = arr.shape
+arr_min = arr.min()
+arr_max = arr.max()
+arr_mean = int(arr.mean())
+arr_out = np.where((arr < arr_mean), 10000, arr)
+driver = gdal.GetDriverByName("GTiff")
+outdata = driver.Create(outFileName, cols, rows, 1, gdal.GDT_UInt16)
+outdata.SetGeoTransform(ds.GetGeoTransform())##sets same geotransform as input
+outdata.SetProjection(ds.GetProjection())##sets same projection as input
+outdata.GetRasterBand(1).WriteArray(arr_out)
+outdata.GetRasterBand(1).SetNoDataValue(10000)##if you want these values transparent
+outdata.FlushCache() ##saves to disk!!
+outdata = None
+band=None
+ds=None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
